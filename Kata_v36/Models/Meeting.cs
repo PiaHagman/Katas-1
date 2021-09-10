5aa706b14ec94f8aac801ef3513545989ef718bd
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2021-09-10 11:19:46 +0200

    Övning DateTime och TimeSpan

--- Kata_v36/Models/Meeting.cs
+++ Kata_v36/Models/Meeting.cs
@@ -29,11 +29,18 @@ namespace Scheduler.Models
             return !(endIsBefore || startIsAfter);
         }
 
-        public override string ToString()
+        public override string ToString()  //Override för att vi vill skapa en egen metod hur vi vill skriva ut
+                                            //vår info. Annars används den ToString som finns i .Net biblioteket
         {
             string date = Start.ToString("d'/'M'/'yy");
 
-            string info = date;
+            string startTime = Start.ToString(format: "H:mm"); //När mötet startar
+            
+            DateTime endTime = Start + Duration; //Resultatet är en DateTime
+
+            string endTimeString = endTime.ToString(format: "H:mm"); //Gör om denna till en string
+
+            string info = $"Datum: {date} Tid: {startTime} - {endTimeString}";
 
             if (Applicant != null)
                 info += " with: " + Applicant.Name;
