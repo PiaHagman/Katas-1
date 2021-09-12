aefe4b5499d8e79eb0808b81fa591047f02b23df
Pia Hagman <pia.hagman@utb.ecutbildning.se>
2021-09-12 14:23:20 +0200

    Lösning RobbersLanguage

--- RobbersLanguage/RobbersLanguage.cs
+++ RobbersLanguage/RobbersLanguage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RobbersLanguageSim
@@ -7,7 +8,36 @@ namespace RobbersLanguageSim
     {
         public static string Encode(string message)
         {
-            throw new NotImplementedException();
+            //Skapar en lista av alla konsonanter
+            List<char> letterList = new List<char>()
+                {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'};
+
+            //Skapar en lista av message
+            List<char> messageList = new List<char>(message);
+
+            string robberMessage = "";
+
+
+            foreach (char c in messageList)
+            {
+                
+                if (letterList.Contains(c))
+                {
+                    //messageList.Add('o');
+                    //messageList.Add(c);
+
+                    robberMessage += c + "o" + c;
+                }
+                else
+                {
+                    robberMessage += c;
+                }
+            }
+
+            //Gör lista till string
+            //string robberMessage=messageList.ToString();
+
+            return robberMessage;
         }
     }
 }
